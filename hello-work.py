bde39f17bf862a88714fd2d35358784d0c8f4ebf
forcrut <79109605+forcrut@users.noreply.github.com>
2021-02-21 18:02:41 +0300

    Update hello-work.py

--- hello-work.py
+++ hello-work.py
@@ -1,3 +1,6 @@
-print("Hello world, Python, work, mistakes!!!")
-for i in range(0,2):
-	print("Держаться, только держаться")
+print("Hello world, Python, work, mistakes!!!")# первая команда вывода текста "Hello world, Python, work, mistakes!!!"
+for i in range(0,2):# цикл for
+	print("Держаться, только держаться\t")
+	for k in "Python":# цикл for , где i будет браться как каждая буква в слове Python
+		print(k, end=" ")# end определяет,что будет находится полсе каждого k элемента
+	print(" \n")# переход на новый абзац
