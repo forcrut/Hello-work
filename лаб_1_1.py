4268cfd28d5d00b584f8246bfb3da82fd777ed15
forcrut <79109605+forcrut@users.noreply.github.com>
2021-03-11 23:12:18 +0300

    Update лаб_1_1.py

--- лаб_1_1.py
+++ лаб_1_1.py
@@ -1,39 +1,50 @@
 #nod
 def nod(arg1, arg2) -> int:
-	if not(isinstance(arg1, int) and isinstance(arg2, int)): #проверка на входящие аргументы , чтобы они имели тип int, иначе ошибка
-		raise ValueError("Value Error")
-	a1 = max(arg1, arg2)
-	b1 = min(arg1, arg2)
+	if type(arg1) != type(int()) or type(arg2) != type(int()): #проверка на входящие аргументы , чтобы они имели тип int, иначе ошибка
+		raise ValueError("Value Error")                      #фактический вызов ошибки Value Error
+	a1, b1, factor, s, myltiply = max(arg1, arg2), min(arg1, arg2), 2, [], 1
 	if a1 % b1 == 0: #без этого if для простых чисел работало не очень хорошо, теперь же будет выдавать 1, если они взаимно просты
-		return(b1)   #или же выдавать простое, если второе делится на него без остатка
-	factor = 2
-	s = []
-	myltiply = 1
+	 	return(b1)   #или же выдавать простое, если второе делится на него без остатка 
 	while factor* factor < max(arg1, arg2):
 		if a1 % factor + b1 % factor == 0: #одновременная проверка a1 и b1 на кратность factor
-			s += [factor]
-			a1 /= factor
-			b1 /= factor
+			myltiply *= factor
+			a1 //= factor
+			b1 //= factor
 		else:
 			factor += 1
-	for i in s:
-		myltiply *= i
 	return(myltiply)
 
 #nok
 def nok(arg1, arg2) ->int:
-	if not(isinstance(arg1, int) and isinstance(arg2, int)): #проверка на входящие аргументы , чтобы они имели тип int, иначе ошибка
-		raise ValueError("Value Error")
-	a1 = max(arg1, arg2)
-	factor = 1
-	while factor* factor < max(arg1, arg2): #пробежка по всем множителям максимального из двух чисел
-		if (a1* factor) % min(arg1, arg2)== 0: #таким образом, если if примет True, то мы нашли нок,
-			return(a1*factor)               #иначе выйдет из цикла while и вернет произведение чисел
+	if type(arg1) != type(int()) or type(arg2) != type(int()) or arg1 == 0 or arg2 == 0: #проверка на входящие аргументы , 
+		raise ValueError("Value Error")         #чтобы они имели тип int  не равны 0, иначе ошибка,фактический вызов ошибки Value Error
+	myltiply, factor, a1, b1 = 1, 2, max(arg1, arg2), min(arg1, arg2)
+	while a1 != 1 or b1 != 1:
+		if a1 % factor == 0 or b1 % factor == 0: # если хотя бы одно из чисел делится на factor, то мы умножаем myltiply на него
+			myltiply *= factor
+			if a1 % factor == 0:# избавление от множителя factor
+				a1 //= factor
+			if b1 % factor == 0:# избавление от множителя factor
+				b1 //= factor
 		else:
-			factor += 1
-	return(arg1* arg2) #если же в while return не сработало, то мы выведем произведение чисел, так как они видимо взаимнопросты,
-                       #или не имеют общих множителей
+			factor += 1 # начинаем поиск дальнейшего множителя myltiply
+	return(myltiply)	
+
+
+
+#nodGauss
+def nodGauss(arg1, arg2) -> int: #нахождение НОД по методу Гаусса a1 = q* a2 + remains; a2 = q1* remains + remains1 ...
+	if type(arg1) != type(int()) or type(arg2) != type(int()): #проверка на входящие аргументы , чтобы они имели тип int, иначе ошибка
+		raise ValueError("Value Error")                      #фактический вызов ошибки Value Error
+	a1, a2, remains = max(arg1, arg2), min(arg1, arg2), 1 
+	while remains != 0:
+		remains, a1=a1 % a2, a2
+		a2 = remains
+	return(a1)
+
+
 
 if __name__ == '__main__':
-	print(nok(70, 24))
-	print(nod(70, 24))
+	print(nok(10, 13))
+	print(nod(1604, 56))
+	print(nodGauss(22,11))
